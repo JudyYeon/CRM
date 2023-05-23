165894bf4b861bd83542ba4c032ef8fbd876ff81
JudyYeon <dev.judykim@gmail.com>
2023-05-23 21:54:58 +0900

    - CustomerProfile, CustomerInfo 컴포넌트 추가 : 이미지 깨짐 (해결)

--- src/components/Customer.js
+++ src/components/Customer.js
@@ -4,8 +4,8 @@ class Customer extends React.Component {
     render() {
         return (
             <div>
-                <CustomerProfile id={this.id} image={this.image} name={this.name} />
-                <CustomerInfo birthday={this.birthday} gender={this.gender} job={this.job} />
+                <CustomerProfile id={this.props.id} image={this.props.image} name={this.props.name} />
+                <CustomerInfo birthday={this.props.birthday} gender={this.props.gender} job={this.props.job} />
             </div>
         );
     }
