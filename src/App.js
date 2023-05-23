eb6e25d205273e0f6b5a72e9c00eafbd835210b2
JudyYeon <dev.judykim@gmail.com>
2023-05-23 22:01:23 +0900

    - 사용자 추가하여 List 형태로 변경

--- src/App.js
+++ src/App.js
@@ -3,28 +3,67 @@ import logo from './logo.svg';
 import './App.css';
 import Customer from './components/Customer'
 
-const customer = {
+const customers = [{
   'id': 1,
-  'image': 'https://placeimg.com/64/64/any',
+  'image': 'https://placeimg.com/64/64/1',
   'name': '김주디',
   'birthday': '961212',
   'gender': '여자',
   'job': '대학생'
-}
+},
+{
+  'id': 2,
+  'image': 'https://placeimg.com/64/64/2',
+  'name': '김태형',
+  'birthday': '951230',
+  'gender': '남자',
+  'job': '배우'
+},
+{
+  'id': 3,
+  'image': 'https://placeimg.com/64/64/3',
+  'name': '이준호',
+  'birthday': '900125',
+  'gender': '남자',
+  'job': '가수'
+}]
 
 
 class App extends Component {
   render() {
     return (
-      <Customer
-        id={customer.id}
-        image={customer.image}
-        name={customer.name}
-        birthday={customer.birthday}
-        gender={customer.gender}
-        job={customer.job}
+      <div>
+        <Customer
+          id={customers[0].id}
+          image={customers[0].image}
+          name={customers[0].name}
+          birthday={customers[0].birthday}
+          gender={customers[0].gender}
+          job={customers[0].job}
 
-      />
+        />
+
+        <Customer
+          id={customers[1].id}
+          image={customers[1].image}
+          name={customers[1].name}
+          birthday={customers[1].birthday}
+          gender={customers[1].gender}
+          job={customers[1].job}
+
+        />
+
+        <Customer
+          id={customers[2].id}
+          image={customers[2].image}
+          name={customers[2].name}
+          birthday={customers[2].birthday}
+          gender={customers[2].gender}
+          job={customers[2].job}
+
+        />
+
+      </div>
     );
   }
 }
--- src/components/Customer.js
+++ src/components/Customer.js
@@ -4,6 +4,7 @@ class Customer extends React.Component {
     render() {
         return (
             <div>
+                {/* jsx :  내부 데이터가 2개 이상인 경우 <div> 태그로 감싸야한다. */}
                 <CustomerProfile id={this.props.id} image={this.props.image} name={this.props.name} />
                 <CustomerInfo birthday={this.props.birthday} gender={this.props.gender} job={this.props.job} />
             </div>
